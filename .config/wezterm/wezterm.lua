c92bbc78358a83f21913ea739123f2720afc827e
Thomas Mees <thomas.mees@overleaf.com>
2024-08-27 17:37:04 +0200

    20240827

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,91 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'SpaceGray Eighties'
+-- config.color_scheme = 'OneDark (base16)'
+-- config.bold_brightens_ansi_colors = 'No' -- change scheme and comment out
+
+-- Matches Ubuntu default terminal
+config.font = wezterm.font('Ubuntu Mono')
+config.font_size = 13.0
+config.freetype_load_target = 'Light'
+-- config.font =  wezterm.font('DejaVu Sans Mono', { weight = 'Book' }) -- vscode?
+
+-- config.window_background_opacity = 0.9
+
+-- Removes the title bar, leaving only the tab bar. Keeps
+-- the ability to resize by dragging the window's edges.
+-- On macOS, 'RESIZE|INTEGRATED_BUTTONS' also looks nice if
+-- you want to keep the window controls visible and integrate
+-- them into the tab bar.
+-- config.window_decorations = 'RESIZE'
+
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+
+local function segments_for_right_status(window, pane)
+  local segments = {}
+  local google_ops = pane:get_user_vars()['google-ops']
+  if not (google_ops == nil or google_ops == 'no') then
+    table.insert(segments, 'google-ops: ' .. google_ops)
+  end
+  -- table.insert(segments, wezterm.strftime('%a %b %-d %H:%M'))
+  table.insert(segments, wezterm.hostname())
+  return segments
+end
+
+wezterm.on('update-status', function(window, pane)
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  local segments = segments_for_right_status(window, pane)
+
+  local color_scheme = window:effective_config().resolved_palette
+  -- Note the use of wezterm.color.parse here, this returns
+  -- a Color object, which comes with functionality for lightening
+  -- or darkening the colour (amongst other things).
+  local bg = wezterm.color.parse(color_scheme.background)
+  local fg = color_scheme.foreground
+
+  -- Each powerline segment is going to be coloured progressively
+  -- darker/lighter depending on whether we're on a dark/light colour
+  -- scheme. Let's establish the "from" and "to" bounds of our gradient.
+  local gradient_to, gradient_from = bg
+  --   if appearance.is_dark() then
+  --     gradient_from = gradient_to:lighten(0.2)
+  --   else
+  --     gradient_from = gradient_to:darken(0.2)
+  --   end
+  gradient_from = gradient_to:lighten(0.2)
+
+  -- Yes, WezTerm supports creating gradients, because why not?! Although
+  -- they'd usually be used for setting high fidelity gradients on your terminal's
+  -- background, we'll use them here to give us a sample of the powerline segment
+  -- colours we need.
+  local gradient = wezterm.color.gradient(
+    {
+      orientation = 'Horizontal',
+      colors = { gradient_from, gradient_to },
+    },
+    #segments -- only gives us as many colours as we have segments.
+  )
+
+  -- We'll build up the elements to send to wezterm.format in this table.
+  local elements = {}
+
+  for i, seg in ipairs(segments) do
+    local is_first = i == 1
+
+    if is_first then
+      table.insert(elements, { Background = { Color = 'none' } })
+    end
+    table.insert(elements, { Foreground = { Color = gradient[i] } })
+    table.insert(elements, { Text = SOLID_LEFT_ARROW })
+
+    table.insert(elements, { Foreground = { Color = fg } })
+    table.insert(elements, { Background = { Color = gradient[i] } })
+    table.insert(elements, { Text = ' ' .. seg .. ' ' })
+  end
+
+  window:set_right_status(wezterm.format(elements))
+end)
+
+return config
--- .zsh/.zshrc
+++ .zsh/.zshrc
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zsh/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 #
 # Executes commands at the start of an interactive session.
 #
@@ -13,6 +20,12 @@ fi
 # Customize to your needs...
 eval "$(rbenv init -)"
 
+# Google Cloud SDK
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/tmees/google-cloud-sdk/path.zsh.inc' ]; then . '/home/tmees/google-cloud-sdk/path.zsh.inc'; fi
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/tmees/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/tmees/google-cloud-sdk/completion.zsh.inc'; fi
+
 # Import aliases
 source $HOME/.aliases
 
@@ -21,3 +34,54 @@ bindkey -M viins 'jj' vi-cmd-mode
 
 # bind backspace to classic delete char (so can append and then delete)
 bindkey -v '^?' backward-delete-char
+
+# This function emits an OSC 1337 sequence to set a user var
+# associated with the current terminal pane.
+# It requires the `base64` utility to be available in the path.
+# This function is included in the wezterm shell integration script, but
+# is reproduced here for clarity
+__wezterm_set_user_var() {
+  if hash base64 2>/dev/null ; then
+    if [[ -z "${TMUX}" ]] ; then
+      printf "\033]1337;SetUserVar=%s=%s\007" "$1" `echo -n "$2" | base64`
+    else
+      # <https://github.com/tmux/tmux/wiki/FAQ#what-is-the-passthrough-escape-sequence-and-how-do-i-use-it>
+      # Note that you ALSO need to add "set -g allow-passthrough on" to your tmux.conf
+      printf "\033Ptmux;\033\033]1337;SetUserVar=%s=%s\007\033\\" "$1" `echo -n "$2" | base64`
+    fi
+  fi
+}
+
+# hooks
+set_env_if_google_ops() {
+    if [ "${PWD##*/}" = "google-ops" ] ; then
+        local google_ops_env=$(gcloud config get project)
+        case $google_ops_env in
+            *ol-prod*)
+                printf "\x1b]11;#660011\x1b\\"
+                __wezterm_set_user_var "google-ops" "production"
+                ;;
+            *ol-stag*)
+                printf "\x1b]11;#110066\x1b\\"
+                __wezterm_set_user_var "google-ops" "staging"
+                ;;
+            *overleaf-ops*)
+                printf "\x1b]11;#353500\x1b\\"
+                __wezterm_set_user_var "google-ops" "ops"
+                ;;
+            *)
+                printf "\x1b]11;#ee0000\x1b\\"
+                echo "bad google-ops env value $google_ops_env"
+                __wezterm_set_user_var "google-ops" "error"
+                ;;
+        esac
+    elif [ "${OLDPWD##*/}" = "google-ops" ] ; then
+        # reset
+        printf "\x1b]111\x1b\\"
+        __wezterm_set_user_var "google-ops" "no"
+    fi
+}
+add-zsh-hook precmd set_env_if_google_ops
+
+# To customize prompt, run `p10k configure` or edit ~/.zsh/.p10k.zsh.
+[[ ! -f ~/.zsh/.p10k.zsh ]] || source ~/.zsh/.p10k.zsh
